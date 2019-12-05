44ab8b1313149e78d4dfd74dcafcb425f8dfa9c8
Tulga <54701267+MongolTulga@users.noreply.github.com>
2019-12-05 15:05:04 +0800

    Add files via upload

---
+++ learning_java/src/com/learning/papi_test/Papi2.java
@@ -0,0 +1,25 @@
+package com.learning.papi_test;
+
+public class Papi2 {
+    int PapiAge;
+    String name = "papi";
+    int age = 15;
+    Papi2( int age,String name ) {
+        System.out.println("她的名字是：" + name);
+    }
+
+    void setAge() {
+        PapiAge = age;
+    }
+
+    void getAge() {
+        System.out.println("她的年龄为：" + PapiAge);
+    }
+
+    public static void main(String[] args) {
+        Papi2 hiPapi = new Papi2(10, "wb");
+        hiPapi.setAge();
+        hiPapi.getAge();
+        System.out.println("她的年龄为：" + hiPapi.PapiAge);
+    }
+}
